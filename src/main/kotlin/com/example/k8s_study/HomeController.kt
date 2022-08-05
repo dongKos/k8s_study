4607de21514a490e5f522f7387dac10136c47456
dongko <dongko@hpcnt.com>
2022-08-05 16:04:59 +0900

    add github action secrets

--- src/main/kotlin/com/example/k8s_study/HomeController.kt
+++ src/main/kotlin/com/example/k8s_study/HomeController.kt
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HomeController {
-
     @GetMapping("/hello")
     fun hello(): String = "world!"
 }
--- src/main/kotlin/com/example/k8s_study/K8sStudyApplication.kt
+++ src/main/kotlin/com/example/k8s_study/K8sStudyApplication.kt
@@ -2,8 +2,6 @@ package com.example.k8s_study
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class K8sStudyApplication
