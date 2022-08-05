1a53777a56a6fd1bd0ff66f7bb083d606ff7f2ee
dongko <dongko@hpcnt.com>
2022-08-05 20:20:41 +0900

    Test: 테스트 실패 시 빌드 실패 하는지 테스트

--- src/test/kotlin/com/example/k8s_study/K8sStudyApplicationTests.kt
+++ src/test/kotlin/com/example/k8s_study/K8sStudyApplicationTests.kt
@@ -1,5 +1,6 @@
 package com.example.k8s_study
 
+import org.assertj.core.api.Fail
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
@@ -8,6 +9,7 @@ class K8sStudyApplicationTests {
 
     @Test
     fun contextLoads() {
+        throw RuntimeException()
     }
 
 }
