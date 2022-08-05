c3ebdff7f3d6522d623116050c191b60ab6b299d
dongko <dongko@hpcnt.com>
2022-08-05 20:29:33 +0900

    Fix: remove fail test

--- src/test/kotlin/com/example/k8s_study/K8sStudyApplicationTests.kt
+++ src/test/kotlin/com/example/k8s_study/K8sStudyApplicationTests.kt
@@ -6,10 +6,4 @@ import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class K8sStudyApplicationTests {
-
-    @Test
-    fun contextLoads() {
-        throw RuntimeException()
-    }
-
 }
